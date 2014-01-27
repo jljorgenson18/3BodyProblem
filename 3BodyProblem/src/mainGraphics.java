469df6ab750af5e8ebb2cee66f2afc7beac06cbb
jljorgenson18 <jljorgenson18@gmail.com>
2014-01-27 17:22:34 -0600

    Gitifier Test

--- 3BodyProblem/src/mainGraphics.java
+++ 3BodyProblem/src/mainGraphics.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class mainGraphics {
 	
 	public final static void main(String[] args) throws IOException {
-		
+		//Gitifier Tets
 		MainFrame frame = new MainFrame("GOGGLES");
                   
     }
